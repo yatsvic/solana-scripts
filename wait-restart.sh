57f7e13e009c41b230a8ac482ccf76288df4f419
Victor Yatsenko <yatsvic>
2022-07-04 23:55:26 +0700

    fix: restart delinq

--- wait-restart.sh
+++ wait-restart.sh
@@ -2,6 +2,10 @@
 
 pushd `dirname ${0}` > /dev/null || exit 1
 source ./env.sh
-solana-validator --ledger ${SOLANA_LEDGER_DIR} wait-for-restart-window
+solana-validator \
+	--ledger ${SOLANA_LEDGER_DIR} \
+	wait-for-restart-window \
+	--max-delinquent-stake 10 \
+        --min-idle-time 10
 popd > /dev/null || exit 1
 
